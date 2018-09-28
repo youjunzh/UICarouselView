a97e126a0a53108f293330a9af24a37225339272
Zhang You Jun  <youjunzh@users.noreply.github.com>
2018-09-28 17:43:41 +0800

    pod lib create

---
+++ Example/Pods/Target Support Files/UICarouselView/UICarouselView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UICarouselView : NSObject
+@end
+@implementation PodsDummy_UICarouselView
+@end
---
+++ Example/UICarouselView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UICarouselView
+//
+//  Created by youjunzh on 09/28/2018.
+//  Copyright (c) 2018 youjunzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
